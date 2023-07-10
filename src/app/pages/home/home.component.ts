fadfa05b79f8741090f3f95b9068ffccacde96b9
Ajay <ajayzyan100@gmail.com>
2023-07-10 08:26:49 +0530

    trending movies completed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MovieApiService } from 'src/app/service/movie-api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private service: MovieApiService) { }
+  
+  bannerResult: any = [];
+  trendingMovies: any = [];
+
+  ngOnInit(): void{
+    
+    this.bannerData();
+
+    this.trendingMovieData();
+
+  }
+
+  //bannerdata
+  bannerData() {
+
+    this.service.bannerApiData().subscribe((result) => {
+
+      this.bannerResult = result.results;
+      
+    })
+
+  }
+
+  //trending movie data
+  trendingMovieData() {
+    
+    this.service.trendingMovieApiData().subscribe((result) => {
+      
+      this.trendingMovies = result.results;
+
+      console.log(result);
+      
+
+    })
+
+  }
 
 }
--- src/app/service/movie-api.service.ts
+++ src/app/service/movie-api.service.ts
@@ -1,10 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  
+  apiKey: string = '42962be0f5de48c9429a95717e9b8e5d';
+
+  //urls
+  bannerApi: string = `${this.baseUrl}/trending/all/week?api_key=${this.apiKey}`;
+
+  trendingMoviesApi:string =  `${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`;
+
+  //banner
+  bannerApiData(): Observable<any>{
+
+    return this.http.get(this.bannerApi);
+
+  }
+
+
+  // trendingmovie
+  trendingMovieApiData(): Observable<any>{
+    
+    return this.http.get(this.trendingMoviesApi);
+
+  }
+
+  
+
+
+
+
 }
